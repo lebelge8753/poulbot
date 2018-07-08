2bcd79ca92b41aab8323afdb3bdcb81399331beb
lebelge8753 <40861911+lebelge8753@users.noreply.github.com>
2018-07-08 21:42:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,13 +11,17 @@ bot.on('ready', function() {
 bot.login(process.env.TOKEN);
 
 
-bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n !help \n !règles");
-    }
-
-    if (message.content === "Salut"){
+bot.on(message.content === "Salut"){
         message.reply("Bien le bonjours, comment ça va? moi ça va et les shasses ça avance bien?. :)");
         console.log("Commande Salut effectué");
     }
+    if (message.content === prefix + "help"){
+        var embed = new Discord.RichEmbed()
+            .setTitle ("Liste des Commandes")
+            .setDescription ("Voici la liste des commandes disponible pour PoulBot")
+            .addField(".Help","Page d'aide", true)
+            .addField("!règles : fait apparaitres les règles du discord", true)
+        message.channel.sendEmbed(embed);
+            
+        
 });
