ce62ddb9073a0f4242fdf8c1be1a1988656fc0b6
lebelge8753 <40861911+lebelge8753@users.noreply.github.com>
2018-07-08 21:54:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,12 +12,12 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "test"){
-        message.channel.sendMessage("test");
-        
-    }(message.content === "Salut"){
-        message.reply("Bien le bonjours, comment ça va? moi ça va et les shasses ça avance bien?. :)");
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n -!help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjours, comment ça va? moi ça va. :)");
         console.log("Commande Salut effectué");
     }
-    
 });
