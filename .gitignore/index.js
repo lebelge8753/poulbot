19d5134b4d3d8ea8aff1e084264bdbe88a3f3c3f
lebelge8753 <40861911+lebelge8753@users.noreply.github.com>
2018-07-14 14:11:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,7 +11,7 @@ bot.on('ready', function() {
 bot.login(process.env.TOKEN);
 
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "accueil").send(`Bienvenue, et que la PouldoShiny soit avec toi ${member}`)
+    member.guild.channels.find("name", "accueil").send(`Bienvenue, et que la PouldoShiny soit avec toi, et passe lire les règles sinon la colére de la Pouldo risque de s'abattre sur toi ${member}`)
 })
 
 bot.on('message', message => {
